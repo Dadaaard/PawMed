6ae1cc003acea01808fe24a78a6fb55de5e58b13
Dadaaard <ramedward322@gmail.com>
2024-09-03 13:05:36 +0800

    removed unecessary code

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -10,24 +10,24 @@ class AppointmentController extends Controller
 {
     //
 
-    public function index()
-    {
-    $appointments = Appointment::select('id', 'Name', 'PetName', 'Purpose', 'conditions', 'symptoms', 'appointmentDate', 'user_id', 'status')->get();
+        public function index()
+        {
+        $appointments = Appointment::select('id', 'Name', 'PetName', 'Purpose', 'conditions', 'symptoms', 'appointmentDate', 'user_id', 'status')->get();
 
 
-    $userIds = $appointments->pluck('user_id')->toArray();
+        $userIds = $appointments->pluck('user_id')->toArray();
 
-    //wala pa nahuman
-    foreach ($appointments as $appointment) {
-        $appointment->user = User::find($appointment->user_id);
-        $appointment->symptoms = explode(',', $appointment->symptoms);
-        $appointment->conditions = explode(',', $appointment->conditions);
-        // $appointment->user = $users->find($appointment->user_id);
-    }
+        //wala pa nahuman
+        foreach ($appointments as $appointment) {
+            $appointment->user = User::find($appointment->user_id);
+            $appointment->symptoms = explode(',', $appointment->symptoms);
+            $appointment->conditions = explode(',', $appointment->conditions);
+            // $appointment->user = $users->find($appointment->user_id);
+        }
 
 
-    return view('Appointment.appointment', compact('appointments'));
-}
+        return view('Appointment.appointment', compact('appointments'));
+        }
 }
 
 
